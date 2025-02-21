5f97a45c56cfb98814a7ca1e541a46a768a16181
Vivian T <150468584+bluesberries@users.noreply.github.com>
2025-02-20 20:30:11 -0800

    Created app with basic layout and prisma models

---
+++ lib/actions/product.actions.ts
@@ -0,0 +1,13 @@
+'use server';
+import { prisma } from "@/db/prisma";
+import { convertToPlainObject } from "../utils";
+import { LATEST_PRODUCTS_LIMIT } from "../constants";
+
+// Get latest products
+export async function getLatestProducts() {
+    const data = await prisma.product.findMany({
+        take: LATEST_PRODUCTS_LIMIT,
+        orderBy: {createdAt:'desc'}
+    });
+    return convertToPlainObject(data);
+}
---
+++ prisma/migrations/20250219003537_init/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `numReview` on the `Product` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Product" DROP COLUMN "numReview",
+ADD COLUMN     "numReviews" INTEGER NOT NULL DEFAULT 0;
