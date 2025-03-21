da5297b69d79f3f273e70054f4ff5910bc861e43
Vivian Tong <199130013+viv-tong@users.noreply.github.com>
2025-03-20 20:08:23 -0700

    Add update user address

--- app/(root)/shipping-address/shipping-address-form.tsx
+++ app/(root)/shipping-address/shipping-address-form.tsx
@@ -1,11 +1,11 @@
 "use client";
 import { useRouter } from "next/navigation";
-import { useSonner } from "sonner";
+import { toast } from "sonner";
 import { useTransition } from "react";
 import { ShippingAddress } from "@/types";
 import { shippingAddressSchema } from "@/lib/validators";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { ControllerRenderProps, useForm } from "react-hook-form";
+import { ControllerRenderProps, useForm, SubmitHandler } from "react-hook-form";
 import { z } from "zod";
 import { shippingAddressDefaultValues } from "@/lib/constants";
 import {
@@ -20,6 +20,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { ArrowRight, Loader } from "lucide-react";
+import { updateUserAddress } from "@/lib/actions/user.action";
 
 const ShippingAddressForm = ({ address }: { address: ShippingAddress }) => {
   const router = useRouter();
@@ -30,9 +31,24 @@ const ShippingAddressForm = ({ address }: { address: ShippingAddress }) => {
   });
 
   const [isPending, startTransition] = useTransition();
-  const onSubmit = (values) => {
-    console.log(values);
-    return;
+  const onSubmit: SubmitHandler<z.infer<typeof shippingAddressSchema>> = async (
+    values
+  ) => {
+    startTransition(async () => {
+      const res = await updateUserAddress(values);
+
+      if (!res.success) {
+        toast.error(res.message, {
+          style: {
+            backgroundColor: "hsl(var(--destructive))",
+            color: "hsl(var(--destructive-foreground))",
+          },
+        });
+        return;
+      }
+
+      router.push("/payment-method");
+    });
   };
   return (
     <>
--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,12 +1,18 @@
 "use server";
 
-import { signInFormSchema, signUpFormSchema } from "../validators";
-import { signIn, signOut } from "@/auth";
+import {
+  shippingAddressSchema,
+  signInFormSchema,
+  signUpFormSchema,
+} from "../validators";
+import { auth, signIn, signOut } from "@/auth";
 // The import doesn't exactly match the course (#34)
 import { isRedirectError } from "next/dist/client/components/redirect-error";
 import { hashSync } from "bcrypt-ts-edge";
 import { prisma } from "@/db/prisma";
 import { formatError } from "../utils";
+import { ShippingAddress } from "@/types";
+import { updateUserAddress } from "@/lib/actions/user.action";
 
 // Sign in the user with credentials
 export async function signInWithCredentials(
@@ -73,4 +79,28 @@ export async function getUserById(userId: string) {
   });
   if (!user) throw new Error("User not found");
   return user;
+}
+
+// Update the user's address
+export async function updateUserAddress(data: ShippingAddress) {
+  try {
+    const session = await auth();
+
+    const currentUser = await prisma.user.findFirst({
+      where: { id: session?.user?.id },
+    });
+
+    if (!currentUser) throw new Error("User not found");
+
+    const address = shippingAddressSchema.parse(data);
+
+    await prisma.user.update({
+      where: { id: currentUser.id },
+      data: { address },
+    });
+
+    return { success: true, message: "User updated successfully" };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
 }
